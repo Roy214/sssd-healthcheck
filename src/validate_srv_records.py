fb0db7f3fc60c7f4d686235fbc916bb5a8c18926
Nikhil Suryawanshi <suryawanshin74@gmail.com>
2022-10-07 16:35:32 +0530

    function to check srv records

--- src/validate_srv_records.py
+++ src/validate_srv_records.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
 
+"""
+* Author : Nikhil Suryawanshi <nsuryawa@redhat.com>
+
+This program will check if srv records for "ldap" and "kerberos" service are resolvable
+
+"""
+import dns.resolver
+import dns.query
+
+domain = input ("Please enter ldap domain:")
+try:
+    dns.resolver.query('_ldap._tcp.'+domain, 'SRV')
+    print ("srv records for ldap are present")
+except:
+    print ("Unable fetch to fecth SRV records for ldap")
+try:
+    dns.resolver.query('_kerberos._tcp.'+domain, 'SRV')
+    print("srv records for kerberos are present")
+except:
+    print ("Unable fetch to fecth SRV records for kerberos")
