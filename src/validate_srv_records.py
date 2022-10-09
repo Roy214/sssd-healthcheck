ddecb8bf3e779ae966b00bb28e29807996a4a048
Nikhil Suryawanshi <suryawanshin74@gmail.com>
2022-10-09 08:46:10 +0530

    defining fucntion for this script

--- src/validate_srv_records.py
+++ src/validate_srv_records.py
@@ -6,23 +6,25 @@
 This program will check if srv records for "ldap" and "kerberos" service are resolvable
 
 """
-import sys
+def validte_srv_records():
 
-sys.path.insert(1, './../py-venv/lib/python3.6/site-packages')
-sys.path.insert(1, './../py-venv/lib64/python3.6/site-packages')
+    import sys
 
-import dns.resolver
-import dns.query
+    sys.path.insert(1, './../py-venv/lib/python3.6/site-packages')
+    sys.path.insert(1, './../py-venv/lib64/python3.6/site-packages')
+
+    import dns.resolver
+    import dns.query
 
 
-domain = input ("Please enter ldap domain:")
-try:
-    dns.resolver.query('_ldap._tcp.'+domain, 'SRV')
-    print ("srv records for ldap are present")
-except:
-    print ("Unable fetch to fecth SRV records for ldap")
-try:
-    dns.resolver.query('_kerberos._tcp.'+domain, 'SRV')
-    print("srv records for kerberos are present")
-except:
-    print ("Unable fetch to fecth SRV records for kerberos")
+    domain = input ("Please enter ldap domain:")
+    try:
+        dns.resolver.query('_ldap._tcp.'+domain, 'SRV')
+        print ("srv records for ldap are present")
+    except:
+        print ("Unable fetch to fecth SRV records for ldap")
+    try:
+        dns.resolver.query('_kerberos._tcp.'+domain, 'SRV')
+        print("srv records for kerberos are present")
+    except:
+        print ("Unable fetch to fecth SRV records for kerberos")
